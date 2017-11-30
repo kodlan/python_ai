7bb6d48d3c8b11aa1a6bba2834ae5e32cb3cfbca
kodlan <kodlan@gmail.com>
2017-11-30 12:41:35 +0200

    import_meta_graph instead of SavedModel, paths updated

--- mnist_keras/test_model.py
+++ mnist_keras/test_model.py
@@ -2,7 +2,9 @@ import tensorflow as tf
 import numpy as np
 import PIL.Image as Image
 
-model_path = "./SavedModel/"
+model_path = "./model/"
+model_meta = "model.ckpt.meta"
+image_path = "./images/"
 input_tensor_name = 'input_1:0'
 output_node_name = 'fc/Softmax:0'
 
@@ -13,6 +15,7 @@ def load_img(img_path):
     data = np.asarray(img, dtype="float32")
     data = np.reshape(data, (1, 28, 28))
     data /= 255
+    data = 1 - data
     return data
 
 
@@ -20,18 +23,23 @@ float_formatter = lambda x: "%.1f" % x
 np.set_printoptions(formatter={'float_kind':float_formatter})
 
 img_data = []
-# img_data.append(load_img("./model/1.jpg"))
-img_data.append(load_img("./model/2.jpg"))
-img_data.append(load_img("./model/3.jpg"))
-img_data.append(load_img("./model/4.jpg"))
-img_data.append(load_img("./model/5.jpg"))
-img_data.append(load_img("./model/6.jpg"))
-img_data.append(load_img("./model/7.jpg"))
-img_data.append(load_img("./model/8.jpg"))
-img_data.append(load_img("./model/9.jpg"))
+img_data.append(load_img(image_path + "1.jpg"))
+img_data.append(load_img(image_path + "2.jpg"))
+img_data.append(load_img(image_path + "3.jpg"))
+img_data.append(load_img(image_path + "4.jpg"))
+img_data.append(load_img(image_path + "5.jpg"))
+img_data.append(load_img(image_path + "6.jpg"))
+img_data.append(load_img(image_path + "7.jpg"))
+img_data.append(load_img(image_path + "8.jpg"))
+img_data.append(load_img(image_path + "9.jpg"))
 
 with tf.Session(graph=tf.Graph()) as sess:
-    tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], export_dir=model_path)
+    # this is not working for some reason
+    # tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], export_dir=model_path)
+
+    saver = tf.train.import_meta_graph(model_path + model_meta)
+    saver.restore(sess, tf.train.latest_checkpoint(model_path))
+
     output = sess.graph.get_tensor_by_name(output_node_name)
     predictions = sess.run(output, {input_tensor_name: img_data})
     print(predictions)
