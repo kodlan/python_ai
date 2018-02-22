c0979d635dbe986eb6a2e2c7d4d40781b15c06a9
kodlan <kodlan@gmail.com>
2018-02-22 14:51:48 +0200

    use h5 instead of weights file

--- yolo/tiny_yolo.py
+++ yolo/tiny_yolo.py
@@ -1,39 +1,13 @@
 import os
-
 import PIL
 from PIL import Image
 from keras import Input, Model
 from keras.layers import Conv2D, BatchNormalization, LeakyReLU, MaxPooling2D, np
 import matplotlib.pyplot as plt
 
-from yolo.tools import yolo_net_out_to_car_boxes
+from yolo.tools import yolo_net_out_to_car_boxes, draw_box
 
 input_size = 416
-
-
-def load_weights(model, yolo_weight_file):
-    tiny_data = np.fromfile(yolo_weight_file, np.float32)[4:]
-
-    index = 0
-    for layer in model.layers:
-        weights = layer.get_weights()
-        if len(weights) > 0:
-            filter_shape, bias_shape = [w.shape for w in weights]
-            if len(filter_shape) > 2:  # For convolutional layers
-                filter_shape_i = filter_shape[::-1]
-                bias_weight = tiny_data[index:index + np.prod(bias_shape)].reshape(bias_shape)
-                index += np.prod(bias_shape)
-                filter_weight = tiny_data[index:index + np.prod(filter_shape_i)].reshape(filter_shape_i)
-                filter_weight = np.transpose(filter_weight, (2, 3, 1, 0))
-                index += np.prod(filter_shape)
-                layer.set_weights([filter_weight, bias_weight])
-            else:  # For regular hidden layers
-                bias_weight = tiny_data[index:index + np.prod(bias_shape)].reshape(bias_shape)
-                index += np.prod(bias_shape)
-                filter_weight = tiny_data[index:index + np.prod(filter_shape)].reshape(filter_shape)
-                index += np.prod(filter_shape)
-                layer.set_weights([filter_weight, bias_weight])
-
 
 input_image = Input(shape=(input_size, input_size, 3))
 
@@ -63,7 +37,7 @@ for i in range(0, 2):
     x = LeakyReLU(alpha=0.1)(x)
 
 model = Model(input_image, x)
-load_weights(model, "tiny-yolo-custom.weight")
+model.load_weights('tiny_yolo_custom.h5')
 
 imagePath = os.path.dirname(os.path.realpath(__file__)) + '/test_images/test1.jpg'
 # image = plt.imread(imagePath)
@@ -81,5 +55,5 @@ boxes = yolo_net_out_to_car_boxes(out[0], threshold = 0.17)
 print (boxes)
 
 f,(ax1,ax2) = plt.subplots(1,2,figsize=(16,6))
-ax1.imshow(image)
+ax1.imshow(img)
 ax2.imshow(draw_box(boxes, plt.imread(imagePath),[[500,1280],[300,650]]))
